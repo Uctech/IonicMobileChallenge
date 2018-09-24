a187cd4fc3f3ffc5ec115055a233ad0a8f019106
Mamah Livinus <livinus134@gmail.com>
2018-09-24 20:43:40 +0100

    Implement color sequence

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  colors: Array<string> =['black','blue','green','cyan','magenta','red','yellow'];
+  grid:Array<Array<string>> = [];
+  constructor() {
+    this.initialize();
   }
 
+  square(i, j){
+    let nextSquare = null;
+    let clickedSquare = document.getElementById(i+''+j);
+    clickedSquare.classList.remove(clickedSquare.classList[0]);
+    if(j == this.colors.length-1){
+      nextSquare = document.getElementById((i+1)%this.colors.length+''+(j+1)%this.colors.length);
+      clickedSquare.classList.add(
+        nextSquare.classList.length > 1
+          ? nextSquare.classList[nextSquare.classList.length-1]
+          : nextSquare.classList[0]);
+    }else if(i != this.colors.length){
+      nextSquare = document.getElementById(i%this.colors.length+''+(j+1)%this.colors.length);
+      clickedSquare.classList.add(
+        nextSquare.classList.length > 1
+          ? nextSquare.classList[nextSquare.classList.length-1]
+          : nextSquare.classList[0]);
+    }
+  }
+
+  initialize(){
+    for(let i=0; i<= this.colors.length; i++){
+      this.grid.push(this.colors);
+    }
+  }
 }
